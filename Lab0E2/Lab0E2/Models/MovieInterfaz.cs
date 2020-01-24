ffec3f814ba32e869f24616795f403edfed0d3b5
diegosalazar00 <57643277+diegosalazar00@users.noreply.github.com>
2020-01-24 12:43:13 -0600

    AddSingleton

---
+++ Lab0E2/Lab0E2/Models/MovieInterfaz.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab0E2.Models
+{
+    interface MovieInterfaz
+    {
+        public void Add(Movie newdata);
+
+        public List<Movie> library();
+    }
+}
