9240d60af9ad0797ce6d9d4525bcc4ab6ab9b63c
Andres Guzman <j.andres.guzman0021@gmail.com>
2020-01-24 12:47:46 -0600

    new branch settings

---
+++ Lab0E2/Lab0E2/Models/MovieSettings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab0E2.Models
+{
+    public class MovieSettings :MovieInterfaz
+    {
+        public List<MovieClass> AllMovies = new List<MovieClass>();
+
+        public void Add(MovieClass newdata)
+        {
+            AllMovies.Add(newdata);
+        }
+        public List<MovieClass> library()
+        {
+            List<MovieClass> getmovies = new List<MovieClass>();
+            if (AllMovies.Count > 10)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    getmovies.Add(AllMovies[AllMovies.Count - i]);
+                }
+            }
+            else
+            {
+                for (int i = 0; i < AllMovies.Count; i++)
+                {
+                    getmovies.Add(AllMovies[i]);
+                }
+            }
+            return getmovies;
+
+        }
+    }
+}
