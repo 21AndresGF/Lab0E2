096e164304b3246633b75762cdba87a531b1502c
diegosalazar00 <57643277+diegosalazar00@users.noreply.github.com>
2020-01-24 12:43:13 -0600

    AddSingleton

---
+++ Lab0E2/Lab0E2/Models/MovieClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab0E2.Models
+{
+    public class MovieClass
+    {
+        public string moviename { get; set; }
+        public string year { get; set; }
+        public string director { get; set; }
+    }
+}
---
+++ Lab0E2/Lab0E2/Models/Singleton.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lab0E2.Models;
+
+namespace Lab0E2.Models
+{
+    public class Singleton
+    {
+        private readonly static Singleton _instance = new Singleton();
+
+        private Singleton()
+        {
+        }
+
+        public static Singleton Instance
+        {
+            get
+            {
+                return _instance;
+            }
+        }
+        public List<MovieClass> MovieClass = new List<MovieClass>();
+    }
+}
